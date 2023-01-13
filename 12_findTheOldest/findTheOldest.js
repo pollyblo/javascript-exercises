5ceea7536c3f1c110b625a2062a1d0959cbcc1a2
pollyblo <ugzcollet@gmail.com>
2023-01-13 19:36:13 +0100

    find the oldest finished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+   for (let people of arr) {
+      if (typeof people.yearOfDeath == 'undefined') {
+         people.yearOfDeath = new Date().getFullYear();
+      }
+      console.log(people.yearOfDeath);
+   }
+   const arrSorted = arr.sort((a, b) => {
+      const lastGuy = a.yearOfDeath - a.yearOfBirth;
+      const nextGuy = b.yearOfDeath - b.yearOfBirth;
+      return lastGuy > nextGuy ? -1 : 1;
+   });
 
+   return arrSorted[0];
 };
 
 // Do not edit below this line
