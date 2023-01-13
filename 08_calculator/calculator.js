d328d5cdd3d02fccb2335b54aedfcdbfa9118156
pollyblo <ugzcollet@gmail.com>
2023-01-13 18:09:21 +0100

    calculator finished

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((acc, cur) => acc + cur, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acc, cur) => acc * cur);
 };
 
-const power = function() {
-	
+const power = function(num, exp) {
+	return num ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num <= 1) return 1;
+	return num * factorial(num - 1);
 };
 
 // Do not edit below this line
