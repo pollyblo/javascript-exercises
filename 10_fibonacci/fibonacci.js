74414a63b1dcd19cdb23277087029a3376c15934
pollyblo <ugzcollet@gmail.com>
2023-01-13 18:59:30 +0100

    fibo finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+   const N = Number(n);
+   if (N < 0) return 'OOPS';
+   else if (N === 1) return 1;
+   else if (N === 0) return 0;
+   else return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
 // Do not edit below this line
