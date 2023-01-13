5ddb18effc2a6c2c894225dfe7c2162ef1269789
pollyblo <ugzcollet@gmail.com>
2023-01-13 18:35:08 +0100

    pals finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+   str = str.toLowerCase().replace(/\W+|_/g, '');
+   return str == str.split('').reverse().join('');
 };
 
 // Do not edit below this line
